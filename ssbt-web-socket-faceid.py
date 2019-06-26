9973a692b6ff27b9d283202b27988b9facdd3ef4
Jose Antonio Jimenez <jantonio.jimenez@gmail.com>
2019-06-26 08:57:18 +0200

    websocket 1st

---
+++ ssbt-web-socket-faceid.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 26 08:52:19 2019
+
+@author: janto
+"""
+
+from simple_websocket_server import WebSocketServer, WebSocket
+
+
+class ssbt_socket_face_id(WebSocket):
+    def handle(self):
+        # echo message back to client
+        self.send_message(self.data)
+    
+    def connected(self):
+        print(self.address, 'connected')
+    
+    def handle_close(self):
+        print(self.address, 'closed')
+
+
+server = WebSocketServer('', 61613, ssbt_socket_face_id)
+server.serve_forever()
